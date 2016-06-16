726f6c8159e14f3314e257f1e64474dd679500df
petterk <petterk@markant.no>
2016-06-16 10:24:56 +0200

    Shipping rules

---
+++ view/frontend/web/js/model/shipping-rates-validation-rules.js
@@ -0,0 +1,19 @@
+/*global define*/
+define(
+    [],
+    function () {
+        "use strict";
+        return {
+            getRules: function() {
+                return {
+                    'postcode': {
+                        'required': true
+                    },
+                    'country_id': {
+                        'required': true
+                    }
+                };
+            }
+        };
+    }
+);
---
+++ view/frontend/web/js/view/shipping-rates-validation.js
@@ -0,0 +1,23 @@
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/shipping-rates-validator',
+        'Magento_Checkout/js/model/shipping-rates-validation-rules',
+        '../model/shipping-rates-validator',
+        '../model/shipping-rates-validation-rules'
+    ],
+    function (
+        Component,
+        defaultShippingRatesValidator,
+        defaultShippingRatesValidationRules,
+        sampleShippingProviderShippingRatesValidator,
+        sampleShippingProviderShippingRatesValidationRules
+    ) {
+        "use strict";
+        defaultShippingRatesValidator.registerValidator('storepickup', sampleShippingProviderShippingRatesValidator);
+        defaultShippingRatesValidationRules.registerRules('storepickup', sampleShippingProviderShippingRatesValidationRules);
+        return Component;
+    }
+);
