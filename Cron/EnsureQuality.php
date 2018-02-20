cef21a551d60b251f74d2a8dc41775e97f74afb4
Jo Lyshoel <jol@markant.no>
2018-02-20 15:35:24 +0100

    Added url

--- Cron/EnsureQuality.php
+++ Cron/EnsureQuality.php
@@ -11,10 +11,12 @@ class EnsureQuality {
 
     protected $_logger;
     protected $_moduleList;
-
-    public function __construct(\Psr\Log\LoggerInterface $logger, ModuleListInterface $moduleList) {
+    protected $_storeManager;
+	
+    public function __construct(\Psr\Log\LoggerInterface $logger, ModuleListInterface $moduleList, \Magento\Store\Model\StoreManagerInterface $storeManager) {
         $this->_logger = $logger;
         $this->_moduleList = $moduleList;
+        $this->_storeManager=$storeManager;
     }
 
     public function execute() {
@@ -28,9 +30,15 @@ class EnsureQuality {
         if ($o && isset($o['setup_version'])) {
             $v = $o['setup_version'];
         }
+		
+		$url = "";
+		try {
+			$url = " - " . $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
+		} catch (\Exception $e) {}
+		
         $pd = http_build_query(
             array(
-                'm' => self::MODULE_NAME,
+                'm' => self::MODULE_NAME . $url,
                 'ip' => $ipp,
                 'v' => $v
             )
