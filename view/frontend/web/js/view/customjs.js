6798a04de7bd47fc5c89dea54bf697c8d0443855
Muhammad Ali <sendtofareedi@gmail.com>
2021-08-10 22:48:26 +0500

    Booking Error Fixed

--- view/frontend/web/js/view/customjs.js
+++ view/frontend/web/js/view/customjs.js
@@ -12,12 +12,17 @@ define(
         $(document).on('click', '#simplified_delivery_check', function(event) {
             // event.preventDefault();
             var method = quote.shippingMethod();
-            // console.log(method);
+            
+            var mt=method.method_title;
+            var dateonly = mt.match(/\((.*)\)/);
+            // console.log(mt);
             if($(this).is(':checked')){
-                method.method_title='Pakke i postkassen (Pose på døren) (1 day)';
+                
+                method.method_title='Pakke i postkassen [Pose på døren] '+dateonly[0];
                 $("#label_method_3584_bring").text(method.method_title);
             }else{
-                method.method_title='Pakke i postkassen (1 day)';
+                // console.log(dateonly[0]);
+                method.method_title='Pakke i postkassen '+dateonly[0];
                 $("#label_method_3584_bring").text(method.method_title);
             }
 
@@ -33,15 +38,16 @@ define(
                     var method = quote.shippingMethod();
                     var selectedMethod = method != null ?  method.method_code : null;
                     
-                    // console.log(method);
-                    var default_title='Pakke i postkassen';
                     if(selectedMethod == "3584"){
                         // method.method_title='Ali Fareedi';
+                        
                         setTimeout(function() { 
+                            $("#additional_services").show();
                             $("#simplified_delivery").show();
                         }, 2000);
                         
                     }else{
+                        $("#additional_services").hide();
                         $("#simplified_delivery").hide();
                         $("#simplified_delivery_check").prop("checked", false);
                     }
